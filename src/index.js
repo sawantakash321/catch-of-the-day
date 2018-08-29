3e102caef3a7ef8ca4c2d00a9b5f0ee822b36e82
Akash Sawant <sawantakash321@gmail.com>
2018-08-29 21:19:49 +0530

    Added components and updated basic skeleton of App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
-import './css/style.css';
 
-render(<StorePicker />, document.querySelector('#main'));
+import './css/style.css';
+import App from './components/App';
+
+render(<App />, document.querySelector('#main'));
