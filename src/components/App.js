66b23e5f39cdacbc21945ab12a427c094b2213f4
Akash Sawant <sawantakash321@gmail.com>
2018-08-30 17:02:26 +0530

    Implemented CRUD

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Inventory from './Inventory';
 import Header from './Header';
 import sampleFishes from '../sample-fishes';
 import Fish from './Fish';
+import base from '../base';
 
 class App extends React.Component {
 	constructor() {
@@ -14,8 +15,42 @@ class App extends React.Component {
 			order: {}
 		};
 		this.addFish = this.addFish.bind(this);
+		this.removeFish = this.removeFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
+		this.removeFromOrder = this.removeFromOrder.bind(this);
+		this.updateFish = this.updateFish.bind(this);
+	}
+
+	componentDidMount() {
+		//this runs right before the <App> is rendered
+		this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+			context: this,
+			state: 'fishes'
+		});
+
+		//check if there is any order in localstorage
+		const localStorageRef = localStorage.getItem(
+			`order-${this.props.params.storeId}`
+		);
+
+		if (localStorageRef) {
+			//update our App component's order state
+			this.setState({
+				order: JSON.parse(localStorageRef)
+			});
+		}
+	}
+
+	componentWillUnmount() {
+		base.removeBinding(this.ref);
+	}
+
+	componentDidUpdate(nextProps, nextState) {
+		localStorage.setItem(
+			`order-${this.props.params.storeId}`,
+			JSON.stringify(nextState.order)
+		);
 	}
 
 	addFish(fish) {
@@ -25,6 +60,18 @@ class App extends React.Component {
 		const timestamp = Date.now();
 		fishes[`fish-${timestamp}`] = fish;
 		//set state
+		this.setState({ fishes });
+	}
+
+	updateFish(key, updatedFish) {
+		const fishes = { ...this.state.fishes };
+		fishes[key] = updatedFish;
+		this.setState({ fishes });
+	}
+
+	removeFish(key) {
+		const fishes = { ...this.state.fishes };
+		fishes[key] = null;
 		this.setState({ fishes });
 	}
 
@@ -44,6 +91,11 @@ class App extends React.Component {
 		this.setState({ order: order }); // can write only order in ES6
 	}
 
+	removeFromOrder(key) {
+		const order = { ...this.state.order };
+		delete order[key];
+		this.setState({ order });
+	}
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -56,12 +108,23 @@ class App extends React.Component {
 								index={key}
 								details={this.state.fishes[key]}
 								addToOrder={this.addToOrder}
-       />
+							/>
 						))}
 					</ul>
 				</div>
-				<Order fishes={this.state.fishes} order={this.state.order} />
-				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
+				<Order
+					removeFromOrder={this.removeFromOrder}
+					params={this.props.params}
+					fishes={this.state.fishes}
+					order={this.state.order}
+				/>
+				<Inventory
+					updateFish={this.updateFish}
+					removeFish={this.removeFish}
+					fishes={this.state.fishes}
+					addFish={this.addFish}
+					loadSamples={this.loadSamples}
+				/>
 			</div>
 		);
 	}
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,11 +1,74 @@
 import React from 'react';
 import AddFishForm from './AddFishForm';
 class Inventory extends React.Component {
+	constructor() {
+		super();
+
+		this.renderInventory = this.renderInventory.bind(this);
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(e, key) {
+		const fish = this.props.fishes[key];
+		const updatedFish = {
+			...fish,
+			[e.target.name]: e.target.value
+		};
+		this.props.updateFish(key, updatedFish);
+	}
+	renderInventory(key) {
+		const fish = this.props.fishes[key];
+		return (
+			<div className="fish-edit" key={key}>
+				<input
+					value={fish.name}
+					type="text"
+					name="name"
+					placeholder="Fish name"
+					onChange={e => this.handleChange(e, key)}
+				/>
+				<input
+					value={fish.price}
+					type="text"
+					name="price"
+					placeholder="Fish price"
+					onChange={e => this.handleChange(e, key)}
+				/>
+				<select
+					value={fish.status}
+					type="text"
+					name="status"
+					placeholder="Fish status"
+					onChange={e => this.handleChange(e, key)}
+				>
+					<option value="available">Fresh!</option>
+					<option value="unavailable">Sold Out!</option>
+				</select>
+				<textarea
+					value={fish.desc}
+					type="text"
+					name="desc"
+					placeholder="Fish desc"
+					onChange={e => this.handleChange(e, key)}
+				/>
+				<input
+					value={fish.image}
+					type="text"
+					name="image"
+					placeholder="Fish image"
+					onChange={e => this.handleChange(e, key)}
+				/>
+				<button onClick={() => this.props.removeFish(key)} />
+			</div>
+		);
+	}
+
 	render() {
 		return (
 			<div>
 				<h2>Inventory</h2>
+				{Object.keys(this.props.fishes).map(this.renderInventory)}
 				<AddFishForm addFish={this.props.addFish} />
+
 				<button onClick={this.props.loadSamples}>Load Sample Fishes</button>
 			</div>
 		);
