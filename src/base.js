b6e28e62c6d525a5029266adb376d9b89372a4b1
Akash Sawant <sawantakash321@gmail.com>
2018-08-30 17:01:36 +0530

    Added Firebase for state sync

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: 'AIzaSyBcUw0py8aeP3WssQuCGAGD14ZdhkGrdAg',
+	authDomain: 'fish-market-akash-sawant.firebaseapp.com',
+	databaseURL: 'https://fish-market-akash-sawant.firebaseio.com'
+});
+
+export default base;
