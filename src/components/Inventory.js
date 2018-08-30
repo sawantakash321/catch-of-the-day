10f6555432468a801c5029d13429a7943c924051
Akash Sawant <sawantakash321@gmail.com>
2018-08-30 17:25:26 +0530

    Added Remove Fish Button

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -57,7 +57,7 @@ class Inventory extends React.Component {
 					placeholder="Fish image"
 					onChange={e => this.handleChange(e, key)}
 				/>
-				<button onClick={() => this.props.removeFish(key)} />
+				<button onClick={() => this.props.removeFish(key)}>Remove Fish</button>
 			</div>
 		);
 	}
